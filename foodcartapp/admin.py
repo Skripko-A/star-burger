5c9cf882161623d5c6f4c7ddd2a8643ed4898d05
a-skripko <skripkoae@gmail.com>
2024-09-03 11:06:46 +0400

    register models Order, OrderProducts

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,10 +3,7 @@ from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 
-from .models import Product
-from .models import ProductCategory
-from .models import Restaurant
-from .models import RestaurantMenuItem
+from .models import Product, Order, OrderProduct, ProductCategory, Restaurant, RestaurantMenuItem
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -104,3 +101,14 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'phonenumber', 'address')
+    inlines = (OrderProductInline,)
+
