b54321aaf18d93fded78c4e93e85f4aa3e096a38
a-skripko <skripkoae@gmail.com>
2024-10-02 13:29:43 +0400

    add error handling for geocoder requests

--- geo_functions.py
+++ geo_functions.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from geopy import distance
@@ -7,20 +8,44 @@ import requests
 from environs import Env
 
 
+logging.basicConfig(
+    filename='geocoder.log',
+    filemode='a',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+logging.debug('Это отладочное сообщение')
+logging.info('Это информационное сообщение')
+logging.warning('Это предупреждающее сообщение')
+logging.error('Это сообщение об ошибке')
+logging.critical('Это критическое сообщение')
+
 env = Env()
 env.read_env()
 
 
 def fetch_coordinates(address):
     apikey = env('YANDEX_GEOCODER_API_KEY')
-    #apikey = 'd9c66e4b-6109-49a0-b8eb-b1ca46f938ab'
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
+    try:
+        response = requests.get(base_url, params={
         "geocode": f"Moscow {address}",
         "apikey": apikey,
         "format": "json",
-    })
-    response.raise_for_status()
+        })
+        response.raise_for_status()
+    except requests.exceptions.ConnectionError:
+        logging.error('Ошибка содеинения при попытке связи с api yandex geocoder')
+    except requests.exceptions.HTTPError(403):
+        logging.error('Проверьте токен доступа к api yandex geocoder')
+    except requests.exceptions.TooManyRedirects:
+        logging.error('Превышено количество запросов к api yandex geocoder')
+    except requests.exceptions.ReadTimeout:
+        logging.error('Превышено время ожидания ответа api yandex geocoder')
+    except requests.exceptions.Timeout:
+        logging.error('Превышено время ожидания ответа api yandex geocoder')
     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
     if not found_places:
