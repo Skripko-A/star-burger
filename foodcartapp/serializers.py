8564aefa097b2180a19d54a82599955172bd69b7
a-skripko <skripkoae@gmail.com>
2024-10-21 13:07:35 +0400

    move order_product creation to OrderSerializer remove needless ListField in OrderProductSerializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -7,20 +7,11 @@ class OrderProductSerializer(ModelSerializer):
     class Meta:
         model = OrderProduct
         fields = ['product', 'quantity']
-    def create(self, validated_data):
-        product = validated_data['product']
-        quantity = validated_data['quantity']
-        price = product.price * quantity
-        return OrderProduct.objects.create(
-            order=Order.objects.get(id=validated_data['new_order'].id),
-            product=product,
-            quantity=quantity,
-            price=price
-        )
+
 
 
 class OrderSerializer(ModelSerializer):
-    products = ListField(child=OrderProductSerializer(), allow_empty=False, write_only=True)
+    products = OrderProductSerializer(many=True)
     class Meta:
         model = Order
         fields = ['firstname','lastname', 'phonenumber', 'address', 'products']
@@ -31,5 +22,16 @@ class OrderSerializer(ModelSerializer):
             phonenumber=validated_data['phonenumber'],
             address=validated_data['address']
         )
+        products_from_request = validated_data.pop('products')
+        for product_data in products_from_request:
+            product = product_data['product']
+            quantity = product_data['quantity']
+            price = product.price * quantity
+            OrderProduct.objects.create(
+                order=new_order,
+                product=product,
+                quantity=quantity,
+                price=price
+            )
         return new_order
     
